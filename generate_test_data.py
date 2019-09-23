51020044933bce3736b71fb8d9205c5430f69ed8
Igor Gorbenko <igor.gorbenko@netwrix.com>
2019-09-23 23:04:34 +0300

    Fix: some changing according to PEP8

--- generate_test_data.py
+++ generate_test_data.py
@@ -1,26 +1,33 @@
 #!/usr/bin/env python3
 
+""" Test data generating """
+
 import os
 import sys
 from random import randrange
 from random import randint, choice
 import uuid
 import datetime
-import time
 
 from utils.monitoring import Monitoring as m
 
+FILE_NAME = "data/transaction_data.csv"
+INITIAL_DATE = datetime.datetime(2015, 1, 1, 12, 00)
+RANDOM_ACCOUNTS = 10
+LIST_TYPE_DEAL = ['commision', 'deal']
+
 
 class TestDataCreator:
-
+    """ The main class for the data generating """
     def __init__(self, num_rows):
-        self.data_file = "data/transaction_data.csv"
-        self.date_in = datetime.datetime(2015, 1, 1, 12, 00)
-        self.list_type_deal = ['commision', 'deal']
+        self.data_file = FILE_NAME
+        self.date_in = INITIAL_DATE
+        self.list_type_deal = LIST_TYPE_DEAL
         self.num_rows = num_rows
 
     @staticmethod
     def random_date(start, num):
+        """ Return a random date from a start value """
         current = start
         while num >= 0:
             curr = current + \
@@ -40,17 +47,15 @@ class TestDataCreator:
             acc_count -= 1
         return acc_list
 
-
     @m.timing
     def create_test_data(self):
-        result = {}
-
+        """ Generating and saving to the file """
         data_file = open(self.data_file, "w")
         date_in = self.date_in
-        list_acc = self.get_accounts_num(10)     # Ten random accounts
+        list_acc = self.get_accounts_num(RANDOM_ACCOUNTS)     # Ten random accounts
 
-        for i in range(self.num_rows):
-            transaction_uid  = uuid.uuid4()
+        for _ in range(self.num_rows):
+            transaction_uid = uuid.uuid4()
             account_uid = choice(list_acc)
             transaction_date = self.random_date(date_in, 0).__next__().strftime("%Y-%m-%d %H:%M:%S")
             type_deal = choice(self.list_type_deal)
@@ -74,8 +79,8 @@ if __name__ == '__main__':
     else:
         num_rows = 1000
 
-    if os.path.exists("data/transaction_data.csv"):
-        os.remove("data/transaction_data.csv")
+    if os.path.exists(FILE_NAME):
+        os.remove(FILE_NAME)
 
     tdc = TestDataCreator(num_rows)
     tdc.create_test_data()
--- utils/monitoring.py
+++ utils/monitoring.py
@@ -1,22 +1,25 @@
+""" Monitoring module """
+
 from functools import wraps
 from time import time
 
 
 class Monitoring:
-
+    """ The main monitoring class """
     @staticmethod
-    def timing(f):
-        @wraps(f)
+    def timing(func):
+        """ Timing show """
+        @wraps(func)
         def wrapper(*args, **kwargs):
             start = time()
-            result = f(*args, **kwargs)
+            result = func(*args, **kwargs)
             end = time()
             time_elapsed = round(end - start, 4)
 
             if result["log_txt"]:
                 print("{}. Elapsed time: {} sec".format(result["log_txt"], time_elapsed))
             else:
-                print("func: {}. Elapsed time: {} sec".format(f.__name__, time_elapsed))
+                print("func: {}. Elapsed time: {} sec".format(func.__name__, time_elapsed))
             return result
         return wrapper
 
